904a86e5fbe7503a7cf7c40022883d5d47bb346e
Alexey Mironov <amironov73@gmail.com>
2019-03-12 08:42:28 +0800

    More code

--- Source/NodeIrbis.ts
+++ Source/NodeIrbis.ts
@@ -2,6 +2,178 @@
 
 module NodeIrbis {
 
+    export const UTF_ENCODING = 'UTF-8';
+    export const ANSI_ENCODING = 'Windows-1251';
+
+    // Статус записи
+
+    export const LOGICALLY_DELETED  = 1;  // Запись логически удалена
+    export const PHYSICALLY_DELETED = 2;  // Запись физически удалена
+    export const ABSENT             = 4;  // Запись отсутствует
+    export const NON_ACTUALIZED     = 8;  // Запись не актуализирована
+    export const LAST_VERSION       = 32; // Последняя версия записи
+    export const LOCKED_RECORD      = 64; // Запись заблокирована на ввод
+
+    // Распространённые форматы
+
+    export const ALL_FORMAT       = "&uf('+0')"; // Полные данные по полям
+    export const BRIEF_FORMAT     = '@brief';    // Краткое библиографическое описание
+    export const IBIS_FORMAT      = '@ibiskw_h'; // Формат IBIS (старый)
+    export const INFO_FORMAT      = '@info_w';   // Информационный формат
+    export const OPTIMIZED_FORMAT = '@';         // Оптимизированный формат
+
+    // Распространённые поиски
+
+    export const KEYWORD_PREFIX    = 'K=';  // Ключевые слова
+    export const AUTHOR_PREFIX     = 'A=';  // Индивидуальный автор, редактор, составитель
+    export const COLLECTIVE_PREFIX = 'M=';  // Коллектив или мероприятие
+    export const TITLE_PREFIX      = 'T=';  // Заглавие
+    export const INVENTORY_PREFIX  = 'IN='; // Инвентарный номер, штрих-код или радиометка
+    export const INDEX_PREFIX      = 'I=';  // Шифр документа в базе
+
+    // Логические операторы для поиска
+
+    export const LOGIC_OR                = 0; // Только ИЛИ
+    export const LOGIC_OR_AND            = 1; // ИЛИ и И
+    export const LOGIC_OR_AND_NOT        = 2; // ИЛИ, И, НЕТ (по умолчанию)
+    export const LOGIC_OR_AND_NOT_FIELD  = 3; // ИЛИ, И, НЕТ, И (в поле)
+    export const LOGIC_OR_AND_NOT_PHRASE = 4; // ИЛИ, И, НЕТ, И (в поле), И (фраза)
+
+    // Коды АРМ
+
+    export const ADMINISTRATOR = 'A'; // Адмнистратор
+    export const CATALOGER     = 'C'; // Каталогизатор
+    export const ACQUSITIONS   = 'M'; // Комплектатор
+    export const READER        = 'R'; // Читатель
+    export const CIRCULATION   = 'B'; // Книговыдача
+    export const BOOKLAND      = 'B'; // Книговыдача
+    export const PROVISITON    = 'K'; // Книгообеспеченность
+
+    /**
+     * Разделитель строк в ИРБИС.
+     */
+    export const IRBIS_DELIMITER = "\x1F\x1E";
+
+    /**
+     * Короткая версия разделителя строк ИРБИС.
+     */
+    export const SHORT_DELIMITER = "\x1E";
+
+    /**
+     * Пустая ли данная строка?
+     *
+     * @param text Строка для изучения.
+     */
+    function isNullOrEmpty(text: string) : boolean {
+        return (text == null) || text == '';
+    }
+
+    /**
+     * Удаление комментариев из строки.
+     *
+     * @param text Текст для удаления комментариев.
+     */
+    function removeComments(text: string) {
+        if (isNullOrEmpty(text)) {
+            return text;
+        }
+
+        if (text.indexOf('/*') < 0) {
+            return text;
+        }
+
+        let result = '';
+        let state = '';
+        let index = 0;
+        let length = text.length;
+
+        while (index < length) {
+            let c = text[index];
+
+            switch (state) {
+                case "'":
+                case '"':
+                case '|':
+                    if (c == state) {
+                        state = '';
+                    }
+
+                    result += c;
+                    break;
+
+                default:
+                    if (c == '/') {
+                        if (index + 1 < length && text[index + 1] == '*') {
+                            while (index < length) {
+                                c = text[index];
+                                if (c == "\r" || c == "\n") {
+                                    result += c;
+                                    break;
+                                }
+
+                                index++;
+                            }
+                        }
+                        else {
+                            result += c;
+                        }
+                    }
+                    else if (c == "'" || c == '"' || c == '|') {
+                        state = c;
+                        result += c;
+                    }
+                    else {
+                        result += c;
+                    }
+                    break;
+            }
+
+            index++;
+        }
+
+        return result;
+    }
+
+    /**
+     * Подготовка динамического формата
+     * для передачи на сервер.
+     *
+     * В формате должны отсутствовать комментарии
+     * и служебные символы (например, перевод
+     * строки или табуляция).
+     *
+     * @param text Текст для обработки.
+     */
+    export function prepareFormat (text: string) {
+        text = removeComments(text);
+        let length = text.length;
+        if (length == 0) {
+            return text;
+        }
+
+        let flag = false;
+        for (let i: number = 0; i < length; i++) {
+            if (text[i] < ' ') {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag) {
+            return text;
+        }
+
+        let result = '';
+        for (let i: number = 0; i < length; i++) {
+            let c = text[i];
+            if (c >= ' ') {
+                result += c;
+            }
+        }
+
+        return result;
+    }
+
     /**
      * Подполе с кодом и значением.
      */
@@ -96,9 +268,17 @@ module NodeIrbis {
             return field;
         }
 
+        public decode(lines: string[]) : void {
+            // TOD implement
+        }
+
+        public encode() : string {
+            // TODO implement
+            return '';
+        }
+
         public validate(throwOnError: boolean = true) : boolean {
             // TODO implement
-
             return true;
         }
     }
@@ -236,7 +416,7 @@ module NodeIrbis {
         public username: string = '';
         public password: string = '';
         public database: string = 'IBIS';
-        public workstation: string = 'C';
+        public workstation: string = CATALOGER;
         public clientId: number = 0;
         public queryId: number = 0;
         public serverVersion: string = '';
@@ -275,7 +455,7 @@ module NodeIrbis {
                 return true;
             }
 
-            AGAIN: while (true) {
+            while (true) {
                 this.clientId = 100000 + Math.floor(Math.random() * 900000);
                 this.queryId = 1;
 
@@ -286,7 +466,7 @@ module NodeIrbis {
                 let response = this.execute(query);
                 response.getReturnCode();
                 if (response.returnCode == -3337) {
-                    continue AGAIN;
+                    continue;
                 }
 
                 if (response.returnCode < 0) {
@@ -395,25 +575,6 @@ module NodeIrbis {
         }
 
         /**
-         * Получение максимального MFN для указанной базы данных.
-         *
-         * @param database Имя базы данных.
-         * @return number Максимальный MFN или 0.
-         */
-        public getMaxMfn(database: string) {
-            if (!this.connected) {
-                return 0;
-            }
-
-            let query = new ClientQuery(this, 'O');
-            query.addAnsi(database);
-            let response = this.execute(query);
-            response.checkReturnCode();
-
-            return response.returnCode;
-        }
-
-        /**
          * Отправка клиентского запроса на сервер
          * и получение от него.
          *
@@ -427,12 +588,188 @@ module NodeIrbis {
         }
 
         /**
+         * Форматирование записи с указанным MFN.
+         *
+         * @param format Текст формата.
+         * @param mfn MFN записи
+         * @throws IrbisException
+         */
+        public formatRecord(format: string, mfn: number) : string {
+            if (!this.connected) {
+                return '';
+            }
+
+            let query = new ClientQuery(this, 'G');
+            query.addAnsi(this.database).newLine();
+            let prepared = prepareFormat(format);
+            query.addAnsi(prepared).newLine();
+            query.add(1).newLine();
+            query.add(mfn).newLine();
+            let response = this.execute(query);
+            response.checkReturnCode();
+            let result = response.readRemainingUtfText();
+
+            return result;
+        }
+
+        /**
+         * Получение максимального MFN для указанной базы данных.
+         *
+         * @param database Имя базы данных.
+         * @throws IrbisException
+         */
+        public getMaxMfn(database: string) : number {
+            if (!this.connected) {
+                return 0;
+            }
+
+            let query = new ClientQuery(this, 'O');
+            query.addAnsi(database);
+            let response = this.execute(query);
+            response.checkReturnCode();
+
+            return response.returnCode;
+        }
+
+        /**
+         * Получение списка файлов.
+         *
+         * @param specification Спецификация.
+         */
+        public listFiles(specification: string) : string[] {
+            if (!this.connected) {
+                return [];
+            }
+            
+            let query = new ClientQuery(this, '!');
+            query.addAnsi(specification).newLine();
+            let response = this.execute(query);
+            let lines = response.readRemainingAnsiLines();
+            let result = [];
+            for (let line of lines) {
+                // TODO implement
+                result.push(line);
+            }
+            
+            return result;
+        }
+
+        /**
          * Получение статуса, подключен ли клиент в настоящее время.
          *
          * @return boolean Подключен?
          */
         public isConnected() : boolean {
             return this.connected;
+        }
+
+        /**
+         * Пустая операция (используется для периодического
+         * подтверждения подключения клиента).
+         *
+         * @return Всегда true при наличии подключения,
+         * т. к. код возврата не анализируется.
+         * Всегда false при отсутствии подключения.
+         */
+        public noOp() : boolean {
+            if (!this.connected) {
+                return false;
+            }
+
+            let query = new ClientQuery(this, 'N');
+            this.execute(query);
+
+            return true;
+        }
+
+        /**
+         * Разбор строки подключения.
+         *
+         * @param connectionString Строка подключения.
+         * @throws IrbisException
+         */
+        public parseConnectionString(connectionString: string) : void {
+            // TODO implement
+        }
+
+        /**
+         * Разблокирование записей.
+         *
+         * @param database База данных.
+         * @param mfnList Массив MFN.
+         * @return bool
+         */
+        public unlockRecords(database : string, mfnList: number[]) : boolean {
+            if (this.connected) {
+                return false;
+            }
+
+            if (mfnList.length == 0) {
+                return true;
+            }
+
+            if (isNullOrEmpty(database)) {
+                database = this.database;
+            }
+
+            let query = new ClientQuery(this, 'Q');
+            query.addAnsi(database).newLine();
+            for (let mfn of mfnList) {
+                query.add(mfn).newLine();
+            }
+
+            this.execute(query);
+
+            return true;
+        }
+
+        /**
+         * Сохранение записи на сервере.
+         *
+         * @param record Запись для сохранения (новая или ранее считанная).
+         * @param lockFlag Оставить запись заблокированной?
+         * @param actualize Актуализировать словарь?
+         * @param dontParse Не разбирать результат.
+         * @throws IrbisException
+         */
+        public writeRecord(record: MarcRecord, lockFlag: number = 0, actualize: number = 1,
+            dontParse: boolean = false) : number {
+            if (!this.connected) {
+                return 0;
+            }
+
+            let database = record.database;
+            if (isNullOrEmpty(database)) {
+                database = this.database;
+            }
+            let query = new ClientQuery(this, 'D');
+            query.addAnsi(database).newLine();
+            query.add(lockFlag).newLine();
+            query.add(actualize).newLine();
+            query.addUtf(record.encode()).newLine();
+            let response = this.execute(query);
+            response.checkReturnCode();
+            if (!dontParse) {
+                record.fields = [];
+                let temp = response.readRemainingUtfLines();
+                let lines = [temp[0]];
+                // lines = array_merge($lines, explode(SHORT_DELIMITER, $temp[1]));
+                record.decode(lines);
+                record.database = database;
+            }
+
+            return response.returnCode;
+        }
+
+        /**
+         * Сохранение текстового файла на сервере.
+         *
+         * @param string $specification Спецификация файла
+         * (включая текст файла).
+         * @return bool
+         */
+        public writeTextFile(specification: string) : void {
+            // TODO implement
         }
     }
 
